6f0f61fb6d640fae587e2bff2d4f9e07cf20d2ca
PRAJJVAL MISHRA <148237654+Prajjval123@users.noreply.github.com>
2024-10-17 23:57:18 +0530

    bug:fixed

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import postRoutes from './routes/post.route.js';
 import notificationRoutes from "./routes/notification.route.js";
 
 dotenv.config();
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
@@ -22,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(express.json({limit: "5mb"}));//to parse req.body'
+app.use(express.json({limit: "5mb"}));//to parse req.body
 //limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 app.use(cookieParser());
